bdfba1ad1e9b96969fff9c223e91f43817c3780c
Ana Lucia Blanco <analuciablanco@outlook.com>
2020-07-08 16:04:37 -0700

    challenge completed!

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.lime.shade400,
         appBar: AppBar(
-          backgroundColor: Colors.orange.shade800,
+          backgroundColor: Colors.lime.shade900,
           title: Text('I Am Poor'),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/sin-dinero.png'),
+          ),
         ),
       ),
     ),
